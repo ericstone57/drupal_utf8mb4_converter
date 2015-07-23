d766186ac193321e1b114c03ca662cb047f05575
infojunkie <karim.ratib@gmail.com>
2015-07-23 15:35:26 -0700

    Batch operations for conversion

--- table_converter.js
+++ table_converter.js
@@ -11,7 +11,7 @@
       if (truncateDanger >= 1) {
         messageString += "<span class='danger'>You have " + truncateDanger + " rows that will be truncated if you convert all. Truncated rows are in red. Please convert them and then refresh the page to convert the remainder with a single click.</span>";
       } else {
-        messageString += "Rows to be converted are shown in green. Primary keys will be truncated to 191 characters but you have no primary key values that are longer than that. <a href='javascript:Drupal.behaviors.table_converter.convertAll()'>Convert All</a>";
+        messageString += "Rows to be converted are shown in green. Primary keys will be truncated to 191 characters but you have no primary key values that are longer than that. <a href='/admin/config/system/tables/convert'>Convert All</a>";
       }
       jQuery(messageString).insertBefore("#table-converter-column-list");
     },
@@ -21,8 +21,5 @@
       jQuery("#" + tablename + "-column-list")
         .load(Drupal.settings.basePath+Drupal.settings.pathPrefix+"admin/config/system/table/"+tablename);
     },
-    convertAll: function(evt) {
-      jQuery("#table-converter-column-list button:enabled").click();
-    }
   }
 })(jQuery)
